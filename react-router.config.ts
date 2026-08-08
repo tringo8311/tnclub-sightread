2462b2ad9c387b4039f3fb0ce7de2ba9f26a5dd7
Tri Ngo <tringo8311@gmail.com>
2026-08-09 00:06:19 +0700

    fix: add basename for React Router on GitHub Pages

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,5 +2,6 @@ import type { Config } from '@react-router/dev/config'
 
 export default {
   appDirectory: 'src/pages',
+  basename: process.env.NODE_ENV === 'production' ? '/tnclub-sightread/' : '/',
   ssr: false, // enable SPA mode
 } satisfies Config
